a5f178e7ac89c7d55448e6a9e44ee98dfce07f06
Diego-Hiriart <hiriart.leon.d@gmail.com>
2021-11-24 17:16:45 -0500

    Saving started

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[Serializable]
+public class Character
+{
+    protected Transform characterTransform;
+    protected float characterHealth;
+}
+
---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[Serializable]
+public class SaveData
+{
+    private PlayerCharacter player;
+    private List<ItemList> items;
+    private List<EnemyList> enemies;
+
+    public SaveData()
+    {
+
+    }
+
+    public SaveData(PlayerCharacter player, List<ItemList> itemlists, List<EnemyList> enemyLists)
+    {
+        this.player = player;
+        foreach (ItemList list in itemlists)
+        {
+            this.items.Add(list);
+        }
+        foreach (EnemyList list in enemyLists)
+        {
+            this.enemies.Add(list);
+        }
+    }
+}
+
